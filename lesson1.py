98e15a8b8507f9399e4d85a3d28d64ada28ce92a
Serg18353 <mikholenok@yandex.ru>
2024-09-09 22:36:01 +0300

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,18 @@
+"1st program"
+print(9**0.5)
+"2nd program"
+print(9.99 > 9.98)
+print(1000 != 1000.1 )
+"3rd program"
+a = (2*2+2)
+print(a)
+b = (2*(2+2))
+print(b)
+print(a==b)
+"4th program"
+a=('123.456')
+b=(float(a))
+print(b)
+print(b * 10)
+print(int((b%1)*10//1))
+
